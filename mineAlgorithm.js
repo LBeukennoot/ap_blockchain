8665b1a00fbf539ca5ba2b0c6800e2c500b8ce8c
LBeukennoot <lars@beukenkamp.net>
2023-02-19 22:35:10 +0100

    removed logs

--- mineAlgorithm.js
+++ mineAlgorithm.js
@@ -8,7 +8,7 @@ const nextBlockLink = "https://programmeren9.cmgt.hr.nl:8000/api/blockchain/next
 
 // stap 1
 
-console.log(mod10sha("text"))
+// console.log(mod10sha("text"))
 // console.log(mod10sha("000078454c038871fa4d67b0022a30baaf25eaa231f8991b108e2624f052f3f8CMGT Mining CorporationBob PIKAB11548689513858154874778871610312"))
 
 
--- mod10sha.js
+++ mod10sha.js
@@ -4,17 +4,15 @@ import { removeSpacesFromArray, check10Multiple, numToSingleValues, charToAscii,
 const mod10sha = (string = "") => {
 
     let stringArray = splitString(string);
-    console.log(stringArray)
 
     // remove spaces from string
     let stringArrayNoSpaces = removeSpacesFromArray(stringArray);
-    console.log(stringArrayNoSpaces)
+
 
     // turn characters into ascii value, except numbers (t == 166)
     let arrayValuesAscii = stringArrayNoSpaces.map(char => {
         return charToAscii(char);
     });
-    console.log(arrayValuesAscii);
 
 
     // split ascii values into single values [1,1,6,...]
@@ -23,33 +21,28 @@ const mod10sha = (string = "") => {
     })
 
     splittedArrayValues = flattenArray(splittedArrayValues);
-    console.log(splittedArrayValues);
 
 
     // split up this array into blocks of 10
     let chunkedArrays = arrayToChunks(splittedArrayValues);
-    console.log(chunkedArrays);
     
 
     // if array length < 10, add 0,1,2,3,... untill length == 10
     const chunkedArrays10multiple = chunkedArrays.map(array => {
         return check10Multiple(array);
     });
-    console.log(chunkedArrays10multiple);
 
 
     // add the first array to the second array, take the modulus of the sum
     const sumArray = sumArrayValues(chunkedArrays10multiple);
-    console.log(sumArray);
 
+    
     // make string of this array (join(""))
     const stringOfSumArray = sumArray.join("");
-    console.log(stringOfSumArray);
     
 
     // do a sha256 over this string
     const hashedString = sha256(stringOfSumArray);
-    console.log(hashedString);
 
 
     //return result
